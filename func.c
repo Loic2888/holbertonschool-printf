a3576f4cc6d2bed558ad66033ce0e9356a38df66
Loic2888 <12188@holbertonstudents.com>
2025-11-25 14:19:24 +0100

    feat: function that print like a real printf

---
+++ func.c
@@ -0,0 +1,80 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * print_char - Prints a character from the argument list.
+ * @args: va_list containing the next argument.
+ * Return: Number of characters printed (always 1).
+ */
+int print_char(va_list args)
+{
+	_putchar(va_arg(args, int));
+	return (1);
+}
+/**
+ * print_string - Prints a string from the argument list.
+ * @args: va_list containing the next argument (a char * string).
+ *
+ * Return: Number of characters printed.
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	if (!str)
+		str = "(NULL)";
+	while (*str)
+	{
+		_putchar(*str++);
+		count++;
+	}
+	return (count);
+}
+/**
+ * print_percent - Prints a percent sign '%'.
+ * @args: va_list (unused).
+ *
+ * Return: Number of characters printed (always 1).
+ */
+int print_percent(va_list args)
+{
+	(void)args;
+	_putchar('%');
+	return (1);
+}
+/**
+ * get_op_func - Retrieves the function associated with a format specifier.
+ * @c: The format specifier character.
+ *
+ * Return: Pointer to the function handling the specifier, or NULL.
+ */
+op_func get_op_func(char c)
+{
+	caracter caract[] = {
+	{'c', print_char},
+	{'s', print_string},
+	{'%', print_percent},
+	};
+
+	int i = 0;
+	int n = sizeof(caract) / sizeof(caracter);
+
+	while (i < n)
+	{
+		if (caract[i].c == c)
+			return (caract[i].op_function);
+		i++;
+	}
+	return (NULL);
+}
--- main.h
+++ main.h
@@ -1,18 +1,39 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * typedef op_func - Function pointer type for handling format specifiers.
+ * @args: va_list containing the next argument to process.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Number of characters printed.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+typedef int (*op_func)(va_list args);
+
+int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+
+op_func get_op_func(char c);
 
 int _printf(const char *format, ...);
+/**
+ * struct caracter - Structure that associates a format specifier
+ *                   character with its corresponding print function.
+ * @c: The format specifier character (e.g., 'c', 's', '%').
+ * @op_function: Function pointer for printing the corresponding argument type.
+ *
+ * Description: This structure is used to map format specifiers
+ * to their respective handling functions in the _printf implementation.
+ */
+struct caracter
+{
+	char c;
+	op_func op_function;
+};
+typedef struct caracter caracter;
 
 #endif
